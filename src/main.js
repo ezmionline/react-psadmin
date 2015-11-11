8fb21a880989a1e9e36b7fd0bfafb7cfdcb5fb36
ezmionline <edward@ezmi.co.uk>
2015-11-11 22:58:44 +0000

    add use strict to main.js within an iffe

--- src/main.js
+++ src/main.js
@@ -3,30 +3,36 @@ var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({
-  render: function(){
-    var Child;
+(function(win){
+  "use strict";
 
-    switch (this.props.route) {
-      case 'about':
-        Child = About;
-        break;
-      default:
-        Child = Home;
+  var App = React.createClass({
+    render: function(){
+      var Child;
+
+      switch (this.props.route) {
+        case 'about':
+          Child = About;
+          break;
+        default:
+          Child = Home;
+      }
+
+      return (
+        <div>
+          <Child />
+        </div>
+      );
     }
+  });
 
-    return (
-      <div>
-        <Child />
-      </div>
-    );
+  function render(){
+    var route = win.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
   }
-});
 
-function render(){
-  var route = window.location.hash.substr(1);
-  React.render(<App route={route} />, document.getElementById('app'));
-}
+  win.addEventListener('hashChange', render);
+  render();
 
-window.addEventListener('hashChange', render);
-render();
+
+})(window);
