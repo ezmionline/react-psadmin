a762f3bd02bcc07ce88fc248b27bcfa2ee569635
ezmionline <edward@ezmi.co.uk>
2015-11-13 19:54:15 +0000

    add mock author api

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  authors:
+  [
+    {
+      id: 'cory-house',
+      firstName: 'Cory',
+      lastName: 'House'
+    },
+    {
+      id: 'scott-allen',
+      firstName: 'Scott',
+      lastName:'Allen'
+    },
+    {
+      id: 'ed-smith',
+      firstName: 'Ed',
+      lastName: 'Smith'
+    }
+  ]
+};
