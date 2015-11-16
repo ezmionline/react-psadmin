42bca2d12ea7f38181d11cab8731250cb7b58527
ezmionline <edward@ezmi.co.uk>
2015-11-16 23:18:31 +0000

    refactor main.js to utilise react router

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,8 @@ var Route = Router.Route;
 var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/homePage')} />
-    <Route name="authors" handler={require('./components/authorPage')} />
-    <Route name="about" handler={require('./components/aboutPage')} />
+    <Route name="authors" handler={require('./components/authors/authorPage')} />
+    <Route name="about" handler={require('./components/about/aboutPage')} />
   </Route>
 );
 
