5e44f4333161f640ce97d0a49a7a13800c7524bd
ezmionline <edward@ezmi.co.uk>
2015-11-21 21:15:09 +0000

    full flux flow for author actions

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -5,6 +5,7 @@ var AuthorApi = require('../api/authorApi');
 var ActionTypes = require('../constants/actionTypes');
 
 var AuthorActions = {
+
   createAuthor: function(author){
     var newAuthor = AuthorApi.saveAuthor(author);
 
@@ -12,7 +13,16 @@ var AuthorActions = {
       actionType: ActionTypes.CREATE_AUTHOR,
       author: newAuthor
     });
+  },
+
+  updateAuthor: function(author){
+    var updatedAuthor = AuthorApi.saveAuthor(author);
+    Dispatcher.dispatch({
+      actionType: ActionTypes.UPDATE_AUTHOR,
+      author: updatedAuthor
+    });
   }
+
 };
 
 module.exports = AuthorActions;
